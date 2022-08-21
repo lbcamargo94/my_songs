94fc903a39a0a2ce3f7f5111b6df183d3bc0b399
Lucas Camargo <lb.camargo94@gmail.com>
2022-08-20 22:14:20 -0300

    feat: ✨ initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/index.css';
+
+function App() {
+  return (
+    <div>
+      <h1>
+        React App!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
