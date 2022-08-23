ce5eb54f05ff01cbecf95301da10ac7ed8bdf57a
Lucas Camargo <lb.camargo94@gmail.com>
2022-08-23 12:45:24 -0300

    ✨ feat(../src/styles): created new file StyledRegister.js

---
+++ src/styles/StyledRegister.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Content = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+  margin: 0;
+  width: 360px;
+  max-width: 640px;
+`
