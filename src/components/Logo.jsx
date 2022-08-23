e326c5f99841ebcb237528589ab274dbb3b210c6
Lucas Camargo <lb.camargo94@gmail.com>
2022-08-22 23:32:02 -0300

    feat: ✨ created new component Logo.jsx

---
+++ src/components/Logo.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Logo() {
+  return (
+    <Box>
+      <TextPrimary primary>
+        Trybe
+      </TextPrimary>
+
+      <TextSecundary >
+        Tunes
+      </TextSecundary>
+    </Box>
+  );
+}
+
+const Box = styled.div`
+  align-items: center;
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  width: 100%;
+`
+
+const TextPrimary = styled.h1`
+  import url('https://fonts.googleapis.com/css2?family=Fascinate+Inline&display=swap');
+  font-size: 2.5rem;
+  font-family: 'Fascinate Inline', cursive;
+  color: #01a101;
+`
+
+const TextSecundary = styled.h1`
+  @import url('https://fonts.googleapis.com/css2?family=Fascinate+Inline&display=swap');
+  font-size: 2.5rem;
+  font-family: 'Fascinate Inline', cursive;
+  color: #f0f8ff;
+`
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -7,23 +7,27 @@ import HeadPhoneBlue from '../assets/images/head-phone-02.jpg';
 import HeadPhoneYellow from '../assets/images/head-phone-03.jpg';
 import HeadPhoneGreen from '../assets/images/head-phone-04.jpg';
 
+// impport component
+import Logo from '../components/Logo';
+
 export default function Home() {
   return (
-    <Home>
+    <Stack>
+      <Logo />
       <Box>
-        <img className="" src={HeadPhonePink} alt="HeadPhonePink" />
-        <img className="" src={HeadPhoneBlue} alt="HeadPhoneBlue" />
-        <img className="" src={HeadPhoneYellow} alt="HeadPhoneYellow" />
-        <img className="" src={HeadPhoneGreen} alt="HeadPhoneGreen" />
+        <img src={HeadPhonePink} alt="HeadPhonePink" />
+        <img src={HeadPhoneBlue} alt="HeadPhoneBlue" />
+        <img src={HeadPhoneYellow} alt="HeadPhoneYellow" />
+        <img src={HeadPhoneGreen} alt="HeadPhoneGreen" />
       </Box>
       <Box>
-
+        
       </Box>
-    </Home>
+    </Stack>
   );
 }
 
-const Home = styled.section`
+const Stack = styled.section`
   align-items: center;
   display: flex;
   flex-direction: column;
@@ -31,6 +35,7 @@ const Home = styled.section`
   justify-content: center;
   width: 100%;
 `
+
 const Box = styled.div`
   align-itens: center;
   border-radios: 1rem;
