e8d2d4d4f1736b1f8a8149cd58cd28c630e8d497
Lucas Camargo <lb.camargo94@gmail.com>
2022-08-23 13:08:05 -0300

    ✨ feat(../src/utils): created new context and provider files

--- src/Index.js
+++ src/Index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// import Aplication
 import App from './App';
+
+// import Provider
+import { TunesProvider } from './utils/provider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <TunesProvider >
     <App />
+  </TunesProvider>
 );
