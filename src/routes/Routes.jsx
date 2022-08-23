87417b919e1cb3cc6442bb70495658c060ce32f0
Lucas Camargo <lb.camargo94@gmail.com>
2022-08-23 11:53:27 -0300

    📝 fix(../src/pages): rename page to pages folder

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom";
-import Home from '../page/Home';
-import Register from '../page/Register';
+import Home from '../pages/Home';
+import Register from '../pages/Register';
 
 export default function NavigationRoutes() {
   return (
